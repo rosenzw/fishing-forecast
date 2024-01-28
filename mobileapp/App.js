ec023652b945b385853e0b1d23af46dfabb6a561
rosenzw <jesseswig@gmail.com>
2024-01-28 09:09:27 -0800

    working end-end with cognito and API Gateway

--- mobileapp/App.js
+++ mobileapp/App.js
@@ -2,10 +2,10 @@ import React, { useEffect, useState } from 'react';
 import {
   StyleSheet,
   Text,
-  View,
-  TextInput,
   Pressable,
-  SafeAreaView
+  SafeAreaView,
+  View,
+  FlatList
 } from 'react-native';
 
 import {
@@ -13,16 +13,34 @@ import {
   useAuthenticator
 } from '@aws-amplify/ui-react-native';
 
-import { Amplify } from 'aws-amplify';
-import amplifyconfig from './src/amplifyconfiguration.json';
+import { Amplify } from "aws-amplify";
+import { fetchAuthSession } from 'aws-amplify/auth';
+import { get } from 'aws-amplify/api';
 
-Amplify.configure(amplifyconfig);
+Amplify.configure({
+    Auth: {
+      Cognito: {
+        userPoolId: process.env.EXPO_PUBLIC_AWS_USER_POOL_ID,
+        userPoolClientId: process.env.EXPO_PUBLIC_AWS_USER_POOL_CLIENT_ID
+      }
+    },
+    API: {
+      REST: {
+        predictions: {
+          endpoint: process.env.EXPO_PUBLIC_AWS_API_ENDPOINT,
+          region: process.env.EXPO_PUBLIC_AWS_API_REGION
+        }
+      }
+    }
+  });
+
 
 // retrieves only the current value of 'user' from 'useAuthenticator'
 const userSelector = (context) => [context.user];
 
 const SignOutButton = () => {
   const { user, signOut } = useAuthenticator(userSelector);
+
   return (
     <Pressable onPress={signOut} style={styles.buttonContainer}>
       <Text style={styles.buttonText}>
@@ -33,9 +51,54 @@ const SignOutButton = () => {
 };
 
 const App = () => {
+
+    const [predictions, setPredictions] = useState([]);
+
+    useEffect(() => {
+        fetchItems();
+    }, []);
+
+  
+    async function fetchItems() {
+      try {
+          const { idToken } = (await fetchAuthSession()).tokens ?? {};
+          console.log(idToken.toString());
+  
+        const restOperation = get({ 
+          apiName: 'predictions',
+          path: '/predictions',
+          options: {
+              headers: { 
+                  Authorization: idToken
+              }
+          }
+        });
+        const response = await restOperation.response;
+        const json = await response.body.json();
+        console.log('GET call succeeded: ', json);
+        setPredictions(json);
+      } catch (error) {
+        console.log('GET call failed: ', error);
+      }
+    }
+    
     return (
         <SafeAreaView style={styles.container}>
             <Text style={styles.title}>Welcome to the Fishing Predictor App!</Text>
+            <FlatList
+            data={predictions}
+            renderItem={({ item }) => (
+                <View style={styles.predictionRow}>
+                    <Text style={styles.headerText}>Location:</Text>
+                    <Text>{item.location}</Text>
+                    <Text style={styles.headerText}>Date:</Text>
+                    <Text>{item.date}</Text>
+                    <Text style={styles.headerText}>Details:</Text>
+                    <Text>{item.details}</Text>
+                </View>
+            )}
+            keyExtractor={item => item.id}
+        />
             <SignOutButton>
             </SignOutButton>
         </SafeAreaView>
@@ -50,7 +113,15 @@ const styles = StyleSheet.create({
       backgroundColor: 'black',
       paddingHorizontal: 8
     },
-    buttonText: { color: 'white', padding: 16, fontSize: 18 }
+    buttonText: { color: 'white', padding: 16, fontSize: 18 },
+    predictionRow: {
+        // Add styles for table rows
+        paddingHorizontal: 20
+    },
+    headerText: {
+        fontWeight: 'bold',
+        // Add other header styles
+    },
   });
 
 export default withAuthenticator(App);
