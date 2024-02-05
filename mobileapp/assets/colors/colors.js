080a410a3d027b7264bebc54c6b7a32f74495a18
rosenzw <jesseswig@gmail.com>
2024-02-04 18:31:32 -0800

    first assets

---
+++ mobileapp/assets/colors/colors.js
@@ -0,0 +1,9 @@
+const colors = {
+    app_background: '#FFFFFF',
+    text_body: '#000000',
+    button_background: '#186DFB',
+    button_text: '#FFFFFF',
+    location_background: '#EEEEEE'
+}
+
+export default colors
